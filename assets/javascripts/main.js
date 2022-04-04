4610d0e83ea70d85f1a1fc531b00f0b013e849ec
Stephen Lenane <info@stephenlenane.com>
2022-04-04 23:56:50 +0100

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,19 @@
-alert("test");
+const english = document.querySelector(".lang-eng");
+const spanish = document.querySelector(".lang-esp");
+const portuguese = document.querySelector(".lang-por");
+
+let header1 = document.querySelector(".header_1");
+
+const updateLanguage = (lang) => {
+  if (lang === "eng") {
+    header1.textContent = "Technical Experience";
+  } else if (lang === "esp") {
+    header1.textContent = "Experiencia Técnica";
+  } else {
+    header1.textContent = "Experiência Técnica";
+  }
+};
+
+english.addEventListener("click", updateLanguage("eng"));
+spanish.addEventListener("click", updateLanguage("esp"));
+portuguese.addEventListener("click", updateLanguage("por"));
