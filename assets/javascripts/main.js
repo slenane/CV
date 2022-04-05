ba863f4dd34484b315f8b510e66f2d1be75f2789
Stephen Lenane <info@stephenlenane.com>
2022-04-05 11:16:23 +0100

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,18 +2,17 @@ const english = document.querySelector(".lang-eng");
 const spanish = document.querySelector(".lang-esp");
 const portuguese = document.querySelector(".lang-por");
 
-let header1 = document.querySelector(".header_1");
+let headers = document.querySelectorAll(".header");
 
 const updateLanguage = (lang) => {
   console.log(lang);
   
-  if (lang === "eng") {
-    header1.textContent = "Technical Experience";
-  } else if (lang === "esp") {
-    header1.textContent = "Experiencia Técnica";
-  } else if (lang === "por") {
-    header1.textContent = "Experiência Técnica";
-  }
+  fetch("./translation.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => console.log(data));
+
 };
 
 english.addEventListener("click", () => {
