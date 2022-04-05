ae83e062aa410bf7ad9423ebaf24db56bb5a9cea
Stephen Lenane <info@stephenlenane.com>
2022-04-05 11:30:28 +0100

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,26 +1,210 @@
-const english = document.querySelector(".lang-eng");
-const spanish = document.querySelector(".lang-esp");
-const portuguese = document.querySelector(".lang-por");
+const language = {
+  english: {
+    bio: 'Freelance full-stack software developer based in Lisbon, Portugal',
+    'link-header': 'Get in touch',
+    website: 'Website',
+    'language-switcher': 'Switch language',
+    'view-on-github': 'View the Project on GitHub',
+    host: 'Hosted on GitHub Pages &mdash; Theme by',
+    headers: [
+      'Techical Experience',
+      'Work Experience',
+      'Languages',
+      'Education',
+    ],
+    experience: [
+      {
+        position: 'Web Developer',
+        'employment-dates': 'Jan 2022 - Present',
+        'employment-description':
+          'During this internship I have been responsible for the support, maintenance, and actualization of the different GoContact websites in addition to SEO optimization. I was tasked with developing [CXBlog](https://www.cxblog.com/) with Elementor, PHP and custom CSS/JavaScript. The site was created to be fully dynamic and require no coding experience to update and manage the site. I was also tasked with creating a Roadshow page to demonstrate the capabilities of the GoContact Contact Center-as-a-service system and create tickets through calls to API endpoints.',
+      },
+      {
+        position: 'Freelance Web Developer',
+        'employment-dates': 'Dec 2020 - Present',
+        'employment-description':
+          'I design and develop custom websites for small businesses as an affordable alternative to expensive drag-and-drop website providers. Through responsive designs I ensure excellent user experience, with a focus on performance, accessibility and best practices to return high SEO scores. I create online stores with the implementation of e-commerce platform payment gateways and custom product templates.',
+      },
+      {
+        position: 'Customer Service Advisor',
+        'employment-dates': 'Apr 2021 - Jan 2022',
+        'employment-description':
+          'Tasked with receiving inbound calls from customers and providing accurate information on the COVID-19 vaccination process through a virtual call center, completing administrative tasks and entering data collated from customers in a timely manner.',
+      },
+      {
+        position: 'English Teacher',
+        'employment-dates': 'Mar 2019 - Apr 2021',
+        'employment-description':
+          'Taught over 5,000 classes through the Cambly platform. This position required strong interpersonal and communication skills to effectively teach grammar points while making classes enjoyable and engaging for students.',
+      },
+    ],
+    'technology-used': 'Technology used',
+    and: 'and',
+    languages: [
+      'English: Native',
+      'Spanish: C1 (advanced)',
+      'Portuguese: A2 (beginner)',
+    ],
+    education: [
+      {
+        location: 'Remote',
+        dates: 'Mar 2021 - Present',
+        'course-focus': [
+          'Hooks, Redux, and React Routing',
+          'Animations',
+          'Next.js',
+        ],
+      },
+    ],
+    'course-focus-header': 'Course focus',
+    'textbooks-read': 'Textbooks read',
+  },
+  spanish: {
+    bio: 'Desarrollador de software full-stack freelance con base en Lisboa, Portugal',
+    'link-header': 'Póngase en contacto',
+    website: 'Sitio web',
+    'language-switcher': 'Cambiar de idioma',
+    'view-on-github': 'Ver el proyecto en GitHub',
+    host: 'Alojado en GitHub Pages &mdash; Tema por',
+    headers: [
+      'Techical Experience',
+      'Experiencia de Trabajo',
+      'Idiomas',
+      'Educación',
+    ],
+    experience: [
+      {
+        position: 'Desarrollador Web',
+        'employment-dates': 'Ene 2022 - Presente',
+        'employment-description':
+          'Durante estas prácticas he sido responsable del soporte, mantenimiento y actualización de los diferentes sitios web de GoContact además de la optimización SEO. Se me encargó el desarrollo de [CXBlog](https://www.cxblog.com/) con Elementor, PHP y CSS/JavaScript personalizado. El sitio fue creado para ser totalmente dinámico y no requerir experiencia en codificación para actualizar y gestionar el sitio. También se me encargó la creación de una página Roadshow para demostrar las capacidades del sistema GoContact Contact Center-as-a-service y crear tickets a través de llamadas a puntos finales de la API.',
+      },
+      {
+        position: 'Desarrollador Web Freelance',
+        'employment-dates': 'Dic 2020 - Presente',
+        'employment-description':
+          'Diseño y desarrollo sitios web personalizados para pequeñas empresas como una alternativa asequible a los costosos proveedores de sitios web de arrastrar y soltar. A través de diseños responsivos aseguro una excelente experiencia de usuario, con un enfoque en el rendimiento, la accesibilidad y las mejores prácticas para devolver altas puntuaciones de SEO. Creo tiendas online con la implementación de pasarelas de pago de plataformas de comercio electrónico y plantillas de productos personalizadas.',
+      },
+      {
+        position: 'Agente de Servicio al Cliente',
+        'employment-dates': 'Abr 2021 - Ene 2022',
+        'employment-description':
+          'Se me encomendó la tarea de recibir llamadas de clientes y proporcionar información precisa sobre el proceso de vacunación COVID-19 a través de un centro de llamadas virtual, completando tareas administrativas e introduciendo los datos cotejados de los clientes de manera oportuna.',
+      },
+      {
+        position: 'Profesor de inglés',
+        'employment-dates': 'Mar 2019 - Abr 2021',
+        'employment-description':
+          'Enseñé más de 5.000 clases a través de la plataforma Cambly. Este puesto requería fuertes habilidades interpersonales y de comunicación para enseñar eficazmente los puntos gramaticales y al mismo tiempo hacer las clases agradables y atractivas para los estudiantes.',
+      },
+    ],
+    'technology-used': 'Tecnología utilizada',
+    and: 'y',
+    languages: [
+      'English: Native',
+      'Spanish: C1 (advanced)',
+      'Portuguese: A2 (beginner)',
+    ],
+    education: [
+      {
+        location: 'Remote',
+        dates: 'Mar 2021 - Present',
+        'course-focus': [
+          'Hooks, Redux, and React Routing',
+          'Animations',
+          'Next.js',
+        ],
+      },
+    ],
+    'course-focus-header': 'Course focus',
+    'textbooks-read': 'Textbooks read',
+  },
+  portuguese: {
+    bio: 'Programador de software freelance baseado em Lisboa, Portugal',
+    'link-header': 'Entre em contacto',
+    website: 'Sítio Web',
+    'language-switcher': 'Mudar de língua',
+    'view-on-github': 'Ver o projecto no GitHub',
+    host: 'Alojado nas Páginas GitHub &mdash; Tema por',
+    headers: [
+      'Techical Experience',
+      'Experiência de Trabalho',
+      'Línguas',
+      'Educação',
+    ],
+    experience: [
+      {
+        position: 'Desenvolvedor Web',
+        'employment-dates': 'Jan 2022 - Presente',
+        'employment-description':
+          'Durante este estágio fui responsável pelo apoio, manutenção e actualização dos diferentes sites da GoContact, para além da optimização de SEO. Fui encarregado de desenvolver [CXBlog](https://www.cxblog.com/) com a Elementor, PHP e CSS/JavaScript personalizado. O site foi criado para ser totalmente dinâmico e não requerer experiência de codificação para actualizar e gerir o site. Também fui incumbido de criar uma página Roadshow para demonstrar as capacidades do sistema GoContact Contact Center-as-a-service e criar bilhetes através de chamadas para pontos finais API.',
+      },
+      {
+        position: 'Desenvolvedor Web Freelancer',
+        'employment-dates': 'Dez 2020 - Presente',
+        'employment-description':
+          'Desenho e desenvolvo sítios web personalizados para pequenas empresas como uma alternativa acessível aos fornecedores de sítios web caros de arrastar e largar. Através de designs reactivos, asseguro uma excelente experiência do utilizador, com foco no desempenho, acessibilidade e melhores práticas para devolver altas pontuações de SEO. Crio lojas online com a implementação de gateways de pagamento de plataformas de comércio electrónico e modelos de produtos personalizados.',
+      },
+      {
+        position: 'Agente de Serviço ao Cliente',
+        'employment-dates': 'Abr 2021 - Jan 2022',
+        'employment-description':
+          'Fui encarregado de receber chamadas recebidas dos clientes e fornecer informações precisas sobre o processo de vacinação COVID-19 através de um call center virtual, completar tarefas administrativas e introduzir atempadamente os dados recolhidos dos clientes.',
+      },
+      {
+        position: 'Professor de Inglês',
+        'employment-dates': 'Mar 2019 - Abr 2021',
+        'employment-description':
+          'Ensinei mais de 5.000 aulas através da plataforma Cambly. Esta posição exigiu fortes capacidades interpessoais e de comunicação para ensinar eficazmente pontos gramaticais, tornando as aulas agradáveis e envolventes para os alunos.',
+      },
+    ],
+    'technology-used': 'Tecnologia utilizada',
+    and: 'e',
+    languages: [
+      'English: Native',
+      'Spanish: C1 (advanced)',
+      'Portuguese: A2 (beginner)',
+    ],
+    education: [
+      {
+        location: 'Remote',
+        dates: 'Mar 2021 - Present',
+        'course-focus': [
+          'Hooks, Redux, and React Routing',
+          'Animations',
+          'Next.js',
+        ],
+      },
+    ],
+    'course-focus-header': 'Course focus',
+    'textbooks-read': 'Textbooks read',
+  },
+};
+
+
+const englishBtn = document.querySelector(".lang-eng");
+const spanishBtn = document.querySelector(".lang-esp");
+const portugueseBtn = document.querySelector(".lang-por");
 
 let headers = document.querySelectorAll(".header");
 
 const updateLanguage = (lang) => {
-  console.log(lang);
+  let current;
+ 
+  if (lang === "english") current = language.english;
+  else if (lang === "spanish") current = language.spanish;
+  else if (lang === "portuguese") current = language.portuguese;
   
-  fetch("/translation.json")
-    .then(response => {
-       return response.json();
-    })
-    .then(data => console.log(data));
+  console.log(current);
 
 };
 
-english.addEventListener("click", () => {
-  updateLanguage("eng")
+englishBtn.addEventListener("click", () => {
+  updateLanguage("english")
 });
-spanish.addEventListener("click", () => {
-  updateLanguage("esp")
+spanishBtn.addEventListener("click", () => {
+  updateLanguage("spanish")
 });
-portuguese.addEventListener("click", () => {
-  updateLanguage("por")
+portugueseBtn.addEventListener("click", () => {
+  updateLanguage("portuguese")
 });
