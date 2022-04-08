587e2b012305536ee2bff0bfb36af9dac6e3a0de
Stephen Lenane <info@stephenlenane.com>
2022-04-08 15:07:33 +0100

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -7,40 +7,32 @@ const language = {
     'language-switcher': 'Switch language',
     'view-on-github': 'View GitHub Repository',
     host: 'Hosted on GitHub Pages — Theme by',
-    headers: [
-      'Techical Experience',
-      'Work Experience',
-      'Languages',
-      'Education',
-    ],
+    headers: ['Techical Experience', 'Languages', 'Education'],
     experience: [
       {
         position: 'Web Developer',
         'employment-dates': 'Jan 2022 - Present',
-        'employment-description':
-          'During this internship I have been responsible for the support, maintenance, and update of the different GoContact websites in addition to SEO optimization. I have worked closely with the design team to develop sites that meet all requirements of functionality and design. I have provided analysis of competitor websites to implement new features in the GoContact website. I was tasked with developing multi-language sites with both WordPress and PHP with custom JavaScript to improve user experience and functionality.',
-        'key-projects':
-          'Developed a dynamic customer experience blog, with articles from experts in the field, to be maintained by an editor with no coding experience and a Roadshow page to demonstrate the capabilities of the GoContact Contact Center-as-a-service system that creates new leads through calls to API endpoints.',
+        'employment-description': `
+          <li>Responsible for the support, maintenance, and update of the different GoContact websites in addition to SEO optimization</li>
+          <li>Worked closely with the design team to develop sites that meet all requirements of functionality and design</li>
+          <li>Provided analysis of competitor websites to implement new features in the GoContact website</li>
+          <li>Developed multi-language sites with both WordPress and PHP with custom JavaScript to improve user experience and functionality</li>
+        `,
+        'key-projects': `
+          <li>Developed <a href="https://www.cxblog.com/">CXBlog</a>, a customer experience blog with articles from 50+ experts across three languages</li>
+          <li>Developed a Roadshow page to demonstrate the capabilities of the GoContact Contact Center-as-a-service system that generates tickets through API calls</li>
+        `,
       },
       {
         position: 'Freelance Web Developer',
         'employment-dates': 'Dec 2020 - Present',
-        'employment-description':
-          'I design and develop custom websites for small businesses as an affordable alternative to expensive drag-and-drop website providers. Through responsive designs I ensure excellent user experience, with a focus on performance, accessibility and best practices to return high SEO scores. I create online stores with the implementation of e-commerce platform payment gateways and custom product templates.',
+        'employment-description': `
+          <li>Design and develop custom websites for small businesses as an affordable alternative to expensive drag-and-drop website providers</li>
+          <li>Ensure excellent user experience through responsive designs, with a focus on performance, accessibility and best practices to yield high SEO scores</li>
+          <li>Create online stores with the implementation of e-commerce platform payment gateways and custom product templates</li>
+        `,
         'key-projects':
           'Designed and developed Sacred Tribe Energy Healing, a personalised e-commerce site based on client\'s vision and halved their monthly website fees. Shopify was integrated to handle secure payments, product inventory and to provide the client with full control of the business. View the <a href="https://github.com/slenane/SacredTribeEnergyHealing">GitHub repository</a> or <a href="https://www.stephenlenane.com">my website</a> for more details',
-      },
-      {
-        position: 'Customer Service Advisor',
-        'employment-dates': 'Apr 2021 - Jan 2022',
-        'employment-description':
-          'Tasked with receiving inbound calls from customers and providing accurate information on the COVID-19 vaccination process through a virtual call center, completing administrative tasks and entering data collated from customers in a timely manner.',
-      },
-      {
-        position: 'English Teacher',
-        'employment-dates': 'Mar 2019 - Apr 2021',
-        'employment-description':
-          'Taught over 5,000 classes through the Cambly platform. This position required strong interpersonal and communication skills to effectively teach grammar points while making classes enjoyable and engaging for students.',
       },
     ],
     'key-projects-header': 'Key projects',
@@ -146,40 +138,32 @@ const language = {
     'language-switcher': 'Cambiar de idioma',
     'view-on-github': 'Ver Repositorio GitHub',
     host: 'Alojado en GitHub Pages — Tema por',
-    headers: [
-      'Experiencia Técnica',
-      'Experiencia de Trabajo',
-      'Idiomas',
-      'Educación',
-    ],
+    headers: ['Experiencia Técnica', 'Idiomas', 'Educación'],
     experience: [
       {
         position: 'Desarrollador Web',
         'employment-dates': 'Ene 2022 - Presente',
-        'employment-description':
-          'Durante estas prácticas he sido responsable del soporte, mantenimiento y actualización de los diferentes sitios web de GoContact además de la optimización SEO. He trabajado estrechamente con el equipo de diseño para desarrollar sitios que cumplan con todos los requisitos de funcionalidad y diseño. He proporcionado análisis de sitios web de la competencia para implementar nuevas características en el sitio web de GoContact. Se me encargó el desarrollo de sitios multilingües tanto con WordPress como con PHP con JavaScript personalizado para mejorar la experiencia del usuario y la funcionalidad.',
-        'key-projects':
-          'Desarrollé un blog dinámico sobre la experiencia del cliente, con artículos de expertos en la materia, que debía mantener un editor sin experiencia en codificación, y una página de Roadshow para demostrar las capacidades del sistema GoContact Contact Center-as-a-service que crea nuevos clientes potenciales a través de llamadas a puntos finales de la API.',
+        'employment-description': `
+          <li>Responsible for the support, maintenance, and update of the different GoContact websites in addition to SEO optimization</li>
+          <li>Worked closely with the design team to develop sites that meet all requirements of functionality and design</li>
+          <li>Provided analysis of competitor websites to implement new features in the GoContact website</li>
+          <li>BOOPBOOOPBOOPDeveloped multi-language sites with both WordPress and PHP with custom JavaScript to improve user experience and functionality</li>
+        `,
+        'key-projects': `
+          <li>Developed <a href="https://www.cxblog.com/">CXBlog</a>, a customer experience blog with articles from 50+ experts across three languages</li>
+          <li>Developed a Roadshow page to demonstrate the capabilities of the GoContact Contact Center-as-a-service system that generates tickets through API calls</li>
+        `,
       },
       {
         position: 'Desarrollador Web Freelance',
         'employment-dates': 'Dic 2020 - Presente',
-        'employment-description':
-          'Diseño y desarrollo sitios web personalizados para pequeñas empresas como una alternativa asequible a los costosos proveedores de sitios web de arrastrar y soltar. A través de diseños responsivos aseguro una excelente experiencia de usuario, con un enfoque en el rendimiento, la accesibilidad y las mejores prácticas para devolver altas puntuaciones de SEO. Creo tiendas online con la implementación de pasarelas de pago de plataformas de comercio electrónico y plantillas de productos personalizadas.',
+        'employment-description': `
+          <li>Design and develop custom websites for small businesses as an affordable alternative to expensive drag-and-drop website providers</li>
+          <li>Ensure excellent user experience through responsive designs, with a focus on performance, accessibility and best practices to yield high SEO scores</li>
+          <li>Create online stores with the implementation of e-commerce platform payment gateways and custom product templates</li>
+        `,
         'key-projects':
-          'Diseñé y desarrollé Sacred Tribe Energy Healing, un sitio de comercio electrónico personalizado basado en la visión del cliente y reduje a la mitad sus tarifas mensuales del sitio web. Se integró Shopify para gestionar los pagos seguros, el inventario de productos y para proporcionar al cliente el control total del negocio. Vea el <a href="https://github.com/slenane/SacredTribeEnergyHealing">repositorio de GitHub</a> o <a href="https://www.stephenlenane.com"mi sitio web</a> para más detalles',
-      },
-      {
-        position: 'Agente de Servicio al Cliente',
-        'employment-dates': 'Abr 2021 - Ene 2022',
-        'employment-description':
-          'Se me encomendó la tarea de recibir llamadas de clientes y proporcionar información precisa sobre el proceso de vacunación COVID-19 a través de un centro de llamadas virtual, completando tareas administrativas e introduciendo los datos cotejados de los clientes de manera oportuna.',
-      },
-      {
-        position: 'Profesor de inglés',
-        'employment-dates': 'Mar 2019 - Abr 2021',
-        'employment-description':
-          'Enseñé más de 5.000 clases a través de la plataforma Cambly. Este puesto requería fuertes habilidades interpersonales y de comunicación para enseñar eficazmente los puntos gramaticales y al mismo tiempo hacer las clases agradables y atractivas para los estudiantes.',
+          'Designed and developed Sacred Tribe Energy Healing, a personalised e-commerce site based on client\'s vision and halved their monthly website fees. Shopify was integrated to handle secure payments, product inventory and to provide the client with full control of the business. View the <a href="https://github.com/slenane/SacredTribeEnergyHealing">GitHub repository</a> or <a href="https://www.stephenlenane.com">my website</a> for more details',
       },
     ],
     'key-projects-header': 'Proyectos clave',
@@ -285,40 +269,32 @@ const language = {
     'language-switcher': 'Mudar de língua',
     'view-on-github': 'Ver Repositório GitHub',
     host: 'Alojado nas Páginas GitHub — Tema por',
-    headers: [
-      'Experiência Técnica',
-      'Experiência de Trabalho',
-      'Línguas',
-      'Educação',
-    ],
+    headers: ['Experiência Técnica', 'Línguas', 'Educação'],
     experience: [
       {
         position: 'Desenvolvedor Web',
         'employment-dates': 'Jan 2022 - Presente',
-        'employment-description':
-          'Durante este estágio fui responsável pelo apoio, manutenção e actualização dos diferentes sites da GoContact, para além da optimização de SEO. Trabalhei em estreita colaboração com a equipa de design para desenvolver sites que satisfazem todos os requisitos de funcionalidade e design. Forneci análises de websites concorrentes para implementar novas funcionalidades no website da GoContact. Fui encarregado de desenvolver sites multilíngues com WordPress e PHP com JavaScript personalizado para melhorar a experiência e funcionalidade do utilizador.',
-        'key-projects':
-          'Desenvolvi um blog dinâmico de experiência do cliente, com artigos de especialistas na área, a ser mantido por um editor sem experiência de codificação e uma página Roadshow para demonstrar as capacidades do sistema GoContact Contact Center-as-a-service que cria novas pistas através de chamadas para pontos terminais API.',
+        'employment-description': `
+          <li>TESTESTESTESTResponsible for the support, maintenance, and update of the different GoContact websites in addition to SEO optimization</li>
+          <li>Worked closely with the design team to develop sites that meet all requirements of functionality and design</li>
+          <li>Provided analysis of competitor websites to implement new features in the GoContact website</li>
+          <li>Developed multi-language sites with both WordPress and PHP with custom JavaScript to improve user experience and functionality</li>
+        `,
+        'key-projects': `
+          <li>Developed <a href="https://www.cxblog.com/">CXBlog</a>, a customer experience blog with articles from 50+ experts across three languages</li>
+          <li>Developed a Roadshow page to demonstrate the capabilities of the GoContact Contact Center-as-a-service system that generates tickets through API calls</li>
+        `,
       },
       {
         position: 'Desenvolvedor Web Freelancer',
         'employment-dates': 'Dez 2020 - Presente',
-        'employment-description':
-          'Desenho e desenvolvo sítios web personalizados para pequenas empresas como uma alternativa acessível aos fornecedores de sítios web caros de arrastar e largar. Através de designs reactivos, asseguro uma excelente experiência do utilizador, com foco no desempenho, acessibilidade e melhores práticas para devolver altas pontuações de SEO. Crio lojas online com a implementação de gateways de pagamento de plataformas de comércio electrónico e modelos de produtos personalizados.',
+        'employment-description': `
+          <li>Design and develop custom websites for small businesses as an affordable alternative to expensive drag-and-drop website providers</li>
+          <li>Ensure excellent user experience through responsive designs, with a focus on performance, accessibility and best practices to yield high SEO scores</li>
+          <li>Create online stores with the implementation of e-commerce platform payment gateways and custom product templates</li>
+        `,
         'key-projects':
-          'Desenhei e desenvolvi o Sacred Tribe Energy Healing, um site de comércio electrónico personalizado baseado na visão do cliente e reduzi para metade as suas taxas mensais no site. Shopify foi integrado para lidar com pagamentos seguros, inventário de produtos e para fornecer ao cliente o controlo total do negócio. Ver o <a href="https://github.com/slenane/SacredTribeEnergyHealing">repositório GitHub</a> ou o <a href="https://www.stephenlenane.com">meu website</a> para mais detalhes',
-      },
-      {
-        position: 'Agente de Serviço ao Cliente',
-        'employment-dates': 'Abr 2021 - Jan 2022',
-        'employment-description':
-          'Fui encarregado de receber chamadas recebidas dos clientes e fornecer informações precisas sobre o processo de vacinação COVID-19 através de um call center virtual, completar tarefas administrativas e introduzir atempadamente os dados recolhidos dos clientes.',
-      },
-      {
-        position: 'Professor de Inglês',
-        'employment-dates': 'Mar 2019 - Abr 2021',
-        'employment-description':
-          'Ensinei mais de 5.000 aulas através da plataforma Cambly. Esta posição exigiu fortes capacidades interpessoais e de comunicação para ensinar eficazmente pontos gramaticais, tornando as aulas agradáveis e envolventes para os alunos.',
+          'Designed and developed Sacred Tribe Energy Healing, a personalised e-commerce site based on client\'s vision and halved their monthly website fees. Shopify was integrated to handle secure payments, product inventory and to provide the client with full control of the business. View the <a href="https://github.com/slenane/SacredTribeEnergyHealing">GitHub repository</a> or <a href="https://www.stephenlenane.com">my website</a> for more details',
       },
     ],
     'key-projects-header': 'Projectos-chave',
@@ -486,7 +462,7 @@ const updateLanguage = (lang) => {
   for (let i = 0; i < positions.length; i++) {
     positions[i].textContent = current.experience[i].position;
     employmentDates[i].textContent = current.experience[i]['employment-dates'];
-    employmentDescriptions[i].textContent =
+    employmentDescriptions[i].innerHTML =
       current.experience[i]['employment-description'];
     if (keyProjects[i] && current.experience[i]['key-projects']) {
       keyProjects[i].innerHTML = current.experience[i]['key-projects'];
